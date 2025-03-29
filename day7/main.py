6235044cfbf927f650aa8a6fdb840b9e5c3832bf
AnthonyShea <agshea25@gmail.com>
2025-03-29 14:29:58 -0600

    functions

--- day7/main.py
+++ day7/main.py
@@ -1,4 +1,28 @@
 import hangman_art_nvim
 import hangman_words_nvim
 
-print("hello")
+def generate_a_random_word():
+  pass
+
+def generate_as_many_blank_letters_in_the_word():
+  pass
+
+def ask_user_to_guess_a_letter():
+  pass
+
+def is_the_guess_letter_in_the_word():
+  pass
+
+def replace_blank_with_letter():
+  pass
+
+def lose_a_life():
+  pass
+
+def are_the_blanks_filled():
+  pass
+
+def have_they_run_out_of_lives():
+  pass
+
+
